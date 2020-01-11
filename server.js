9aaea6481c972844a4e8a2002824b1d44f99e9f2
gedeyandi456 <gede.yandi@blockchainzoo.com>
2020-01-11 15:22:37 +0800

    deployment

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+
+const app = express()
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log('DB CONNECTION ERROR: ', err))
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(cors({ origin: process.env.CLIENT_URL }))
+}
+
+app.use('/api', authRoutes)
+app.use('/api', userRoutes)
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`API is running on port ${port}`)
+})
